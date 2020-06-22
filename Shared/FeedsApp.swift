c6ae61e8baf599850dc658f1acc12c30b7ea07aa
Mark Malström <mark@malstrom.me>
2020-06-22 18:26:46 -0500

    Initial Commit

---
+++ Shared/FeedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedsApp.swift
+//  Shared
+//
+//  Created by Mark Malstrom on 6/22/20.
+//
+
+import SwiftUI
+
+@main
+struct FeedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
