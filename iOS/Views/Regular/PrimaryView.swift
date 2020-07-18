e6f3f35df9177a3b0a43359ae7a5c27505c1f502
Mark Malström <mark@malstrom.me>
2020-07-18 18:10:37 -0500

    Fixed iPad waterfall grid

--- iOS/Views/Regular/PrimaryView.swift
+++ iOS/Views/Regular/PrimaryView.swift
@@ -5,21 +5,40 @@ struct PrimaryView: View {
     @EnvironmentObject var store: AppStore
     var isLoading: Binding<Bool> { store.binding(for: \.isLoading) { .setIsLoading($0) } }
     
-    let columns = [GridItem](repeating: .init(.fixed(343), spacing: 24, alignment: .top), count: 3)
-    
-    private var dateFormatter: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.timeStyle = .none
-        formatter.dateStyle = .full
-        formatter.locale = Locale(identifier: "en_US")
-        formatter.doesRelativeDateFormatting = false
-        return formatter
+    // TODO: Sort from left to right, top to bottom; currently sorts top to bottom, left to right
+    var feedArray: [[Post]] {
+        var counter = 0
+        var tmp = [Post]()
+        return store.state.todayFeed.compactMap {
+            // FIXME: Check this math
+            let number = store.state.todayFeed.count / 3
+            // TODO: Calculate how to divide the array to have about even view column heights, based on the predicted content size
+            
+            guard store.state.todayFeed.last != $0 else {
+                tmp.append($0)
+                return tmp
+            }
+            
+            guard counter != number else {
+                counter = 0
+                let returnable = tmp
+                tmp = []
+                return returnable
+            }
+            
+            counter += 1
+            tmp.append($0)
+            return nil
+        }
     }
     
+    let columns = [GridItem](repeating: .init(.fixed(343), spacing: 24, alignment: .top), count: 3)
+    
     var body: some View {
+        // FIXME: The scrolling performance is not great
         ScrollView {
 //            VStack(alignment: .leading) {
-//                Text(dateFormatter.string(from: Date()).uppercased())
+//                Text("Friday, June 17".uppercased())
 //                    // FIXME: Make this a semantic computed property
 //                    .fontWeight(.semibold)
 //                    .fontSize(13)
@@ -31,9 +50,15 @@ struct PrimaryView: View {
 //                    .fontSize(34)
 //            }.padding()
             
-            LazyVGrid(columns: columns, spacing: 48) {
-                ForEach(store.state.homeFeed) { post in
-                    Card(post: post)
+            LazyVStack {
+                HStack(alignment: .top) {
+                    ForEach(0..<feedArray.count, id: \.self) { array in
+                        LazyVStack {
+                            ForEach(feedArray[array]) { post in
+                                Card(post: post)
+                            }
+                        }
+                    }
                 }
             }.padding(48)
         }
